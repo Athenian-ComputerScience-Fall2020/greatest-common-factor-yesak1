7570b1f07ce954be6e9bf0c79e402fc447bb4c85
yesak1 <23kkazliner@athenian.org>
2020-09-14 19:04:31 +0000

    finished..

--- my_code.py
+++ my_code.py
@@ -1,19 +1,42 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  I did probably 90% on my own but I didn't exactly know how functions worked so I refreshed my memory by looking it up. I also needed help with while loops
 
-def find_gcf(x,y):   # Do not change function name!
+def find_gcf(x,y): 
+    if x > y:
+        z=x%y
+        if z==0:
+            return y
+    if x<y:
+        z=y%x
+        if z==0:
+            return x
+        else: 
+            return find_gcf(x,z)
+    if x==y:
+        return x
+    if x==0:
+        return y
+    if y==0:
+        return x
+    while z > 0:
+        if x % z == 0 and y % z == 0:
+            return z
+        z -= 1
+
+          # Do not change function name!
     # User code goes here
 
 
-    return gcf
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(find_gcf(6,9))
+    x = int(input("Enter a number: "))
+    y = int(input("Enter another number: "))
+    
+    print(find_gcf(x,y))
 
     # After you are satisfied with your results, use input() to prompt the user for two values:
-    #x = int(input("Enter a number: "))
-    #y = int(input("Enter another number: "))
+    
 
